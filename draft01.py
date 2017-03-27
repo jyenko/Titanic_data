ac2c69be3cdd9096ff96cd97483f30515721aa94
Jeremy J. Yenko <jyenko.ny@outlook.com>
2017-03-27 10:30:56 -0400

    Update draft01.py
    Update the python script to reflect changes made in the Juptyer Notebook

--- draft01.py
+++ draft01.py
@@ -14,7 +14,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#Phase 1 - Read and process csv data
 #Read in csv data using only relevant columns, as indicated by usecols()
 df = pd.read_csv('titanicdata.csv', header=0, names=['PassengerID', 'Survived', 'Class',
                                                      'Sex', 'Fare'], usecols=(0,1,2,4,9))
@@ -25,7 +24,6 @@ print df.isnull().sum()
 #Check data types
 print "Raw data types:"
 print df.dtypes
-print "---------------------------------------"
 
 #Fix data types
 def fix_data_types(col, newtype):
@@ -35,151 +33,71 @@ fix_data_types('PassengerID', object) #Change the PassengerID column to an objec
 fix_data_types('Survived', bool) #Change the Survived column to a bool type
 
 #Confirm data types
-print "--------------------------------------"
 print "New data types:"
 print df.dtypes
 
-print "--------------------------------------"
-
+#Compute relevant statistics for the entire dataframe
 print "Overall statistics:"
 print df.describe()
 
-print "--------------------------------------"
-
+#Compute relevant statistics for the fare column
 print "Overall fare statistics:"
-
 print df['Fare'].describe()
 
+#Create a boxplot showing the distribution of fares for all passengers in the dataset
 fig1 = plt.figure()
 plt.boxplot(df['Fare'])
 fig1.suptitle('Boxplot distribution of all fares')
 plt.ylabel('Fare')
 plt.show()
 
-
-print "--------------------------------------"
-
+#Create groupby() objects for the survived and class columns
 survivors = df.groupby(['Survived'])
 cabin = df.groupby(['Class'])
-gender = df.groupby(['Sex'])
 
-print "--------------------------------------"
-
+#Compute the median fare for survivors and non survivors
 print "Median fare based on survival status"
 print survivors['Fare'].median()
-print "++++++++++++++++++++++++++++++++++++++"
 
+#Compute the mean fare for survivors 
 print "Mean fare based on survival status"
 print survivors['Fare'].mean()
-print "++++++++++++++++++++++++++++++++++++++"
 
+#Display the number of survivors and the number of non survivors
 print survivors.size()
-print "======================================"
 
+#Display the mean fare for each class
 print "Mean fare based on class"
 print cabin['Fare'].mean()
-print "======================================"
 
+#Display the median fare for each class
 print "Median fare based on class"
 print cabin['Fare'].median()
-print "======================================"
 
+#Display the number of passengers in each class
 print cabin.size()
-print "======================================"
 
-print "--------------------------------------"
+#Display the percentage of survivors by class
+print df.groupby(['Class'])['Survived'].mean()
 
-first_class = df.query('Class==1')
-first_class_survivors = first_class.query('Survived==1')
-first_survived = float(len(first_class_survivors))
-first_total = float(len(first_class))
-first_pct = (first_survived/first_total)*100
-print "Approximately ", int(round(first_pct)), " percent of first class passengers survived"
-print first_survived
-print first_total
+#Display the percentage of survivors by gender
+print df.groupby(['Sex'])['Survived'].mean()
 
-second_class = df.query('Class==2')
-second_class_survivors = second_class.query('Survived==1')
-second_survived = float(len(second_class_survivors))
-second_total = float(len(second_class))
-second_pct = (second_survived/second_total)*100
-print "Approximately ", int(round(second_pct)), " percent of second class passengers survived"
-print second_survived
-print second_total
+#Display the total number of male and female passengers in the dataset
+print df.groupby(['Sex']).size()
 
-third_class = df.query('Class==3')
-third_class_survivors = third_class.query('Survived==1')
-third_survived = float(len(third_class_survivors))
-third_total = float(len(third_class))
-third_pct = (third_survived/third_total)*100
-print "Approximately ", int(round(third_pct)), " percent of third class passengers survived"
-print third_survived
-print third_total
-
-print "-----------------------------------------"
-
-gender = df.groupby(['Sex'])
-
-print gender.size()
-
-women = df.query('Sex=="female"')
-female_survivors = women.query('Survived==1')
-female_survived = float(len(female_survivors))
-female_total = float(len(women))
-female_pct = (female_survived/female_total)*100
-print "Approximately ", int(round(female_pct)), " percent of female passengers survived"
-print int(female_survived), " out of ", int(female_total)
-
-men = df.query('Sex=="male"')
-male_survivors = men.query('Survived==1')
-male_survived = float(len(male_survivors))
-male_total = float(len(men))
-male_pct = (male_survived/male_total)*100
-print "Approximately ", int(round(male_pct)), " percent of male passengers survived"
-print int(male_survived), " out of ", int(male_total)
-
-print "+++++++++++++++++++++++++++++++++++++++++"
-
-pas_survived = df.query('Survived==1')['Fare']
-pas_perished = df.query('Survived==0')['Fare']
-
+#Create a histogram showing the fare distributions for survivors and non survivors simultaneously
+pas_survived = df.query('Survived==1')['Fare'] #Use the query() method to get the fare column for survivors
+pas_perished = df.query('Survived==0')['Fare'] #Use the query() method to get the fare column for non survivors
 fig2 = plt.figure()
-plt.hist(pas_survived, bins=100)
+plt.hist(pas_survived, bins=100) #Set the number of bins to 100 for the histogram
 plt.hist(pas_perished, bins=100)
-fig2.suptitle('Distribution of fares by survival status')
-plt.xlabel('Fare')
-plt.ylabel('Number of occurences')
-plt.legend(("Survived", "Perished"))
+fig2.suptitle('Distribution of fares by survival status') #Set the title of the chart
+plt.xlabel('Fare') #Label the x axis as Fare
+plt.ylabel('Number of occurences') #Label the y axis as Number of occurences
+plt.legend(("Survived", "Perished")) #Create a legend to distinguish each distribution
 plt.show()
 
-survived = df.query('Survived==1')['Fare'].values
-perished = df.query('Survived==0')['Fare'].values
-                   
-fig3 = plt.figure()                   
-plt.boxplot(perished)
-fig3.suptitle('Boxplot distribution of fares for non survivors')
-plt.ylabel('Fare')
-plt.ylim([0, 550])
-plt.show()
-
-fig4 = plt.figure()
-plt.boxplot(survived)
-fig4.suptitle('Boxplot distribution of fares for survivors')
-plt.ylabel('Fare')
-plt.ylim([0, 550])
-plt.show()
-
+#Create a boxplot showing the fare distribution by survival status
 sns.boxplot(data=df, y='Fare', x='Survived')
-
-
-
-
-
-
-
-
-
-
-
-
-
+sns.plt.show()
